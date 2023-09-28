01bbdf6389557171a8d6e26d5fe47ee177812fc1
RoseDeLaAnne <mwexcellency@outlook.com>
2023-09-28 12:28:08 +0300

    0.0.5

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,10 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const newsTexts = document.querySelectorAll(".news-text");
+  const mailingForm = document.querySelector(".mailing__form");
+  const modalThanks = document.querySelector(".modal_thanks");
+  const openMenu = document.getElementById('open-menu')
+  const menu = document.querySelector('.menu')
 
   // Define the maximum character limit
   const maxChars = 200;
@@ -12,5 +16,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const truncatedText = newsText.textContent.slice(0, maxChars) + "...";
       newsText.textContent = truncatedText;
     }
+  });
+
+  openMenu.addEventListener('click', () => {
+    menu.classList.add('show')
+  })
+
+  mailingForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission behavior
+    modalThanks.classList.add("show");
+    document.querySelector(".modal-background").classList.add("show");
+    document.body.style.overflow = "hidden";
   });
 });
