c4ee830198bc0339157bd5a567ffb8f34a0edb51
RoseDeLaAnne <mwexcellency@outlook.com>
2023-09-30 16:41:47 +0300

    0.0.9

--- app/js/app.js
+++ app/js/app.js
@@ -2,29 +2,27 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 document.addEventListener("DOMContentLoaded", () => {
-  const newsTexts = document.querySelectorAll(".news-text");
+  const city = document.querySelector('.city')
+  const searchIcon = document.getElementById('search-icon');
+  const cInputAdaptive = document.querySelector('.c-input-adaptive')
   const mailingForm = document.querySelector(".mailing__form");
   const modalThanks = document.querySelector(".modal_thanks");
   const openMenu = document.getElementById('open-menu')
   const menu = document.querySelector('.menu')
   const imSearchingInput = document.getElementById('im-searching')
 
+  searchIcon.addEventListener('click', () => {
+    city.classList.add('hide')
+    searchIcon.classList.add('hide')
+    cInputAdaptive.classList.add('block')
+  })
+
   imSearchingInput.addEventListener('focus', function () {
     document.getElementById('pop-up_calculate-the-cost-desktop').classList.add('block')
   });
-  imSearchingInput.addEventListener('blur', function () {
-    document.getElementById('pop-up_calculate-the-cost-desktop').classList.remove('block')
-  });
-
-  // Define the maximum character limit
-  const maxChars = 200;
-
-  newsTexts.forEach((newsText) => {
-    if (newsText.textContent.length > maxChars) {
-      const truncatedText = newsText.textContent.slice(0, maxChars) + "...";
-      newsText.textContent = truncatedText;
-    }
-  });
+  // imSearchingInput.addEventListener('blur', function () {
+  //   document.getElementById('pop-up_calculate-the-cost-desktop').classList.remove('block')
+  // });
 
   openMenu.addEventListener('click', () => {
     menu.classList.add('show')
